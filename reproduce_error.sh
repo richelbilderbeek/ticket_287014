7ab4ce687577fd38790e910d0312bb2efcb28539
richelbilderbeek <richel@richelbilderbeek.nl>
2024-02-21 09:32:21 +0100

    Proper error message

--- reproduce_error.sh
+++ reproduce_error.sh
@@ -10,4 +10,13 @@ export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 # ERROR: Cannot find Java or it's a wrong version -- please make sure that Java 8 or later (up to 18) is installed
 # export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 
-nextflow -c metontiime_9feb.conf run metontiime2_9feb.nf
+# Original
+# nextflow -c metontiime_9feb.conf run metontiime2_9feb.nf
+
+# Suitable for local
+nextflow -c metontiime_9feb_local.conf run metontiime2_9feb.nf
+
+#  --workDir trimmed_and_filtered_Q15_qz \
+#  --dbSequencesFasta sh_refs_qiime_ver9_dynamic_all_25.07.2023.fasta \
+#  --dbTaxonomyTsv="noheader_taxonomy_qiime_ver9_dynamic_alleukaryotes_25.07.2023.tsv"
+#  --resultsDir=Output
