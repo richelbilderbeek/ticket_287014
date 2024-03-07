171b0b99f67fae48b65c0252ba69e4c81574f7c0
richelbilderbeek <richel@richelbilderbeek.nl>
2024-03-07 10:14:01 +0100

    Use user data

--- run_rackham.sh
+++ run_rackham.sh
@@ -13,17 +13,21 @@ sed -i 's/ 10.GB / 7.GB /' metontiime2.conf
 # Change the executor from pbspro to local
 sed -i "s/executor = 'pbspro'/executor = 'local'/" metontiime2.conf
 
-work_dir="${PWD}/work_local_singularity"
+# work_dir="${PWD}/work_local_singularity"
+work_dir="/proj/staff/richel/ticket_287014/work_local_singularity"
 echo "work_dir: ${work_dir}"
 
 # Get the sequences in the correct folder 
 mkdir "${work_dir}"
-cp example_reads.fastq.gz "${work_dir}"
+# cp example_reads.fastq.gz "${work_dir}"
+cp /proj/naiss2023-22-866/MetONTIIME/trimmed_and_filtered_Q15/seqkitQ15cutadaptlib1sup_bc01.fastq "${work_dir}"
 
-results_dir="${PWD}/results_local_singularity"
+# results_dir="${PWD}/results_local_singularity"
+results_dir="/proj/staff/richel/ticket_287014/results_local_singularity"
 echo "results_dir: ${results_dir}"
 
-db_sequence_fasta_filename="${PWD}/example_db_sequence.fasta"
+# db_sequence_fasta_filename="${PWD}/example_db_sequence.fasta"
+db_sequence_fasta_filename="/crex/proj/naiss2023-22-866/MetONTIIME/sh_refs_qiime_ver9_dynamic_all_25.07.2023.fasta"
 echo "db_sequence_fasta_filename: ${db_sequence_fasta_filename}"
 
 sample_metadata_tsv_filename="${work_dir}/created_sample_metadata.tsv"
@@ -34,7 +38,8 @@ sample_metadata_tsv_filename="${work_dir}/created_sample_metadata.tsv"
 # sample_metadata_tsv_filename="${PWD}/example_sample_metadata.tsv"
 echo "sample_metadata_tsv_filename: ${sample_metadata_tsv_filename}"
 
-taxonomy_tsv_filename="${PWD}/example_taxonomy.tsv"
+#taxonomy_tsv_filename="${PWD}/example_taxonomy.tsv"
+taxonomy_tsv_filename="/crex/proj/naiss2023-22-866/MetONTIIME/noheader_taxonomy_qiime_ver9_dynamic_alleukaryotes_25.07.2023.tsv"
 echo "taxonomy_tsv_filename: ${taxonomy_tsv_filename}"
 
 # Run locally
